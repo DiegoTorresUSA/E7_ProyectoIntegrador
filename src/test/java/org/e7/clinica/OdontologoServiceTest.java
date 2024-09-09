937c80b885881bfabf69cc82da435680f7866eda
diegoT <mando25676@gmail.com>
2024-09-09 16:32:00 -0500

    ADD test de pruebas al proyecto

--- src/test/java/org/e7/clinica/OdontologoServiceTest.java
+++ src/test/java/org/e7/clinica/OdontologoServiceTest.java
@@ -1,37 +1,54 @@
 package org.e7.clinica;
 
+import org.e7.clinica.entity.Odontologo;
+import org.e7.clinica.entity.Paciente;
+import org.e7.clinica.service.impl.OdontologoService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@Transactional
 class OdontologoServiceTest {
-   /* static org.slf4j.Logger logger = LoggerFactory.getLogger(OdontologoServiceTest.class);
+    @Autowired
+    OdontologoService odontologoService;
+    Odontologo odontologo;
+    Odontologo odontologo1;
+    Odontologo odontologoDesdeDB;
 
-    @BeforeAll
-    static void crearTabla() {
-        Connection connection = null;
-        try {
-            Class.forName("org.h2.Driver");
-            connection = DriverManager.getConnection("jdbc:h2:./clinica;INIT=RUNSCRIPT FROM 'create.sql'", "sa", "sa");
+    @BeforeEach
+    void crearOdontologo(){
+        odontologo = new Odontologo();
+        odontologo.setNombre("aleja");
+        odontologo.setApellido("osorio");
+        odontologo.setMatricula("3");
+        odontologoDesdeDB = odontologoService.guardarOdontologo(odontologo);
 
-        }catch (Exception e) {
-            logger.error(e.getMessage());
-        }finally {
-            try {
-                connection.close();
-            } catch (SQLException ex) {
-                logger.error(ex.getMessage());
-            }
-        }
+        odontologo1 = new Odontologo();
+        odontologo1.setNombre("Diego");
+        odontologo1.setApellido("Torres");
+        odontologo1.setMatricula("4");
+        odontologoDesdeDB = odontologoService.guardarOdontologo(odontologo1);
     }
 
     @Test
     @DisplayName("Testeamos el insert a la Base de Datos")
     void testInsert() {
         //dado
-        Odontologo odontologo = new Odontologo("b1", "Alejandra", "Osorio");
         //cuando
-        Odontologo odontologoDB = odontologoService.guardarOdontologo(odontologo);
         //Entonces
-        assertNotNull(odontologoDB.getMatricula());
+        assertNotNull(odontologoDesdeDB.getMatricula());
 
     }
 
@@ -42,9 +59,32 @@ class OdontologoServiceTest {
         List<Odontologo> listaOdontologos = new ArrayList<>();
         // cuando
         listaOdontologos = odontologoService.buscarTodos();
-        logger.info(String.valueOf(listaOdontologos));
         // entonces
         assertNotNull(listaOdontologos);
-    }*/
+        System.out.println(listaOdontologos);
+    }
     //hacer test para modificar y eliminar
+
+    @Test
+    @DisplayName("Testing para obtener un odontologo en BD")
+    void testIdBD(){
+        //dado
+        Integer id = 1;
+        // cuando
+        odontologoDesdeDB = odontologoService.buscarPorId(id).get();
+        // entonces
+        assertEquals(id, odontologoDesdeDB.getId());
+    }
+
+    /*@Test
+    @DisplayName("Testing para eliminar un odontologo en BD")
+    void testDelete(){
+        //dado
+        Integer id = 2;
+        // cuando
+        odontologoDesdeDB = odontologoService.buscarPorId(id).get();
+        odontologoService.eliminarOdontologo(id);
+        // entonces
+        assertEquals(id.);
+    }*/
 }
--- src/test/java/org/e7/clinica/PacienteServiceTest.java
+++ src/test/java/org/e7/clinica/PacienteServiceTest.java
@@ -1,24 +1,50 @@
 package org.e7.clinica;
 
-class PacienteServiceTest {
-   /* static Logger logger = LoggerFactory.getLogger(PacienteServiceTest.class);
-    PacienteService pacienteService = new PacienteService(new PacienteDaoH2());
 
-    @BeforeAll
-    static void tablas(){
-        H2Connection.crearTablas();
+import org.e7.clinica.entity.Domicilio;
+import org.e7.clinica.entity.Paciente;
+import org.e7.clinica.service.impl.PacienteService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@Transactional
+
+class PacienteServiceTest {
+    @Autowired
+    PacienteService pacienteService;
+    Paciente paciente;
+    Paciente pacienteDesdeDb;
+
+    @BeforeEach
+    void crearPaciente(){
+        Domicilio domicilio = new Domicilio(null, "falsa", 456, "Alamos", "bogotá");
+        paciente = new Paciente();
+        paciente.setApellido("Torres");
+        paciente.setNombre("Diego");
+        paciente.setDni("80189301");
+        paciente.setFechaIngreso(LocalDate.of(2024, 9,9));
+        paciente.setDomicilio(domicilio);
+        pacienteDesdeDb = pacienteService.guardarPaciente(paciente);
     }
 
     @Test
     @DisplayName("Testear que un paciente se guarde en la base de datos con su domicilio")
     void caso1(){
         //dado
-        Paciente paciente = new Paciente("Romero","Luciana", "56655", LocalDate.of(2024, 7, 16),
-                new Domicilio("Falsa", 456, "Cipolleti", "Rio Negro"));
         // cuando
-        Paciente pacienteDesdeDB = pacienteService.guardarPaciente(paciente);
         // entonces
-        assertNotNull(pacienteDesdeDB.getId());
+        assertNotNull(pacienteDesdeDb.getId());
     }
 
     @Test
@@ -27,8 +53,9 @@ class PacienteServiceTest {
         //dado
         Integer id = 1;
         // cuando
-        Paciente paciente = pacienteService.buscarPorId(id);
+        Paciente paciente = pacienteService.buscarPorId(id).get();
         // entonces
-        assertEquals(id, paciente.getId());*/
+        assertEquals(id, paciente.getId());
     }
+}
 //hacer test para modificar y eliminar
