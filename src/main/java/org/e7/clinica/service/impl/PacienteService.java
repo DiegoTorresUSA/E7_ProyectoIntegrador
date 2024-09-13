e12d759dbcc41f8716f314410d2e76d9b7234ea7
dtorres <diego.torres@transmilenio.gov.co>
2024-09-12 20:18:27 -0500

    ADD loggers en métodos de paciente

--- src/main/java/org/e7/clinica/service/impl/PacienteService.java
+++ src/main/java/org/e7/clinica/service/impl/PacienteService.java
@@ -21,15 +21,19 @@ public class PacienteService implements IPacienteService {
     }
     @Override
     public Paciente guardarPaciente(Paciente paciente) {
-        return pacienteRepository.save(paciente);
+        Paciente paciente1 = pacienteRepository.save(paciente);
+        logger.info("paciente1 "+paciente1.getId() +  " guardado");
+        return paciente1;
     }
 
     @Override
     public Optional<Paciente> buscarPorId(Integer id) {
         Optional<Paciente> paciente = pacienteRepository.findById(id);
         if (paciente.isPresent()) {
+            logger.info("paciente encontrado: "+ paciente.get());
             return paciente;//Repository.findById(id);
         } else {
+            logger.error("El paciente con ID  " + id + "  no fue encontrado");
             throw new ResourceNotFoundException("El paciente con ID  " + id + "  no fue encontrado");
         }
     }
@@ -38,17 +42,22 @@ public class PacienteService implements IPacienteService {
     public List<Paciente> buscarTodos() {
         List<Paciente> pacientes=pacienteRepository.findAll();
         if(pacientes.isEmpty()){
+            logger.error("No se encontraron pacientes");
             throw new ResourceNotFoundException("No se encontraron pacientes");
         }
-        return pacienteRepository.findAll();
+        List<Paciente> pacienteDesdeDb = pacienteRepository.findAll();
+        logger.info("Pacientes encontrados: "+ pacienteDesdeDb.size());
+        return pacienteDesdeDb;
     }
 
     @Override
     public void modificarPaciente(Paciente paciente) {
         Optional<Paciente> pacienteEncontrado = pacienteRepository.findById(paciente.getId());
         if(pacienteEncontrado.isPresent()){
+            logger.info("Paciente encontrado, actualizando: " + paciente.getId());
             pacienteRepository.save(paciente);
         } else {
+            logger.info("El paciente  no fue encontrado");
             throw new ResourceNotFoundException("El paciente  no fue encontrado");
         }
     }
@@ -58,18 +67,9 @@ public class PacienteService implements IPacienteService {
         Optional<Paciente> pacienteEncontrado = pacienteRepository.findById(id);
         if(pacienteEncontrado.isPresent()){
             pacienteRepository.deleteById(id);
+            logger.info("Paciente "+pacienteEncontrado.get() + " eliminado");
         } else {
             throw new ResourceNotFoundException("El paciente  con ID "+ id +" no fue encontrado");
         }
     }
-
-    /*@Override
-    public List<Paciente> buscarPorApellidoyNombre(String apellido, String nombre) {
-        return pacienteRepository.findAllById(apellido, nombre);
-    }
-
-    @Override
-    public List<Paciente> buscarLikeNombre(String nombre) {
-        return pacienteRepository.findByNombreLike(nombre);
-    }*/
 }
